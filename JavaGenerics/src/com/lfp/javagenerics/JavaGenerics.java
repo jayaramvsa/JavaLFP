626748a4de6bf628b73a35841c6f56b73c4134a7
jayaramvsa <jayaramvsa@gmail.com>
2021-04-05 17:28:56 +0530

    Modified files for finding max Float and Test Cases for the same

--- JavaGenerics/src/com/lfp/javagenerics/JavaGenerics.java
+++ JavaGenerics/src/com/lfp/javagenerics/JavaGenerics.java
@@ -3,16 +3,44 @@ package com.lfp.javagenerics;
 public class JavaGenerics {
 
     public static void main(String[] args) {
-        Integer xInt = 70,yInt = 4,zInt=9;
-        Integer intMax = maximumInteger(xInt,yInt,zInt);
-        System.out.println("***The value of X : "+ xInt + " \n ***The Value of Y :" + yInt + "\n ***The Value of Z :" +zInt +
+        Integer xInt = 70, yInt = 4, zInt = 9;
+        Integer intMax = maximumInteger(xInt, yInt, zInt);
+        System.out.println("***The value of X : " + xInt + " \n ***The Value of Y :" + yInt + "\n ***The Value of Z :" + zInt +
                 "\n *** The Maximum Value among x,y,z is : " + intMax);
+        // For Float Exercise
+        Float xFloat = 1.3f, yFloat = 2.3f, zFloat = 9.5f;
+        Float fMax = maximumFloat(xFloat, yFloat, zFloat);
+        System.out.println("***The value of X : " + xFloat + " \n ***The Value of Y :" + yFloat + "\n ***The Value of Z :" + zFloat +
+                "\n *** The Maximum Value among x,y,z is : " + fMax);
     }
 
+    /**
+     * <p>Method for checking Maximum Integer in tradition way without generics</p>
+     *
+     * @param xInt
+     * @param yInt
+     * @param zInt
+     * @return
+     */
     public static Integer maximumInteger(Integer xInt, Integer yInt, Integer zInt) {
         Integer iMax = xInt;//Assumption X is maximum
-        if(yInt.compareTo(xInt) > 0) iMax = yInt; // if Y > X set Y as Maximum
-        if(zInt.compareTo(iMax) >0 ) iMax = zInt; // if Z  > Maximum then set Z as Maximum
+        if (yInt.compareTo(xInt) > 0) iMax = yInt; // if Y > X set Y as Maximum
+        if (zInt.compareTo(iMax) > 0) iMax = zInt; // if Z  > Maximum then set Z as Maximum
         return iMax;
+    }
+
+    /**
+     * <p>Method for checking Maximum Float in tradition way without generics</p>
+     *
+     * @param xFloat
+     * @param yFloat
+     * @param zFloat
+     * @return
+     */
+    public static Float maximumFloat(Float xFloat, Float yFloat, Float zFloat) {
+        Float maxFloat = xFloat;//Assumption X is Maximum
+        if (yFloat.compareTo(xFloat) > 0) maxFloat = yFloat; // If Y > X set Y as Maximum
+        if (zFloat.compareTo(maxFloat) > 0) maxFloat = zFloat; // if Z > Maximum set Z as Maximum
+        return maxFloat;
     }
 }
--- JavaGenerics/src/com/lfp/javagenerics/JavaGenericsTest.java
+++ JavaGenerics/src/com/lfp/javagenerics/JavaGenericsTest.java
@@ -5,24 +5,48 @@ import org.junit.jupiter.api.Test;
 
 public class JavaGenericsTest {
 
+    // Test Cases for Integer
     @Test
     public void validXMax() {
         Integer iX = 5,iY=2, iZ = 1;
-        Integer imax = JavaGenerics.maximumInteger(iX,iY,iZ);
-        Assertions.assertEquals(iX,imax);
+        Integer iMax = JavaGenerics.maximumInteger(iX,iY,iZ);
+        Assertions.assertEquals(iX,iMax);
     }
 
     @Test
     public void validYMax() {
         Integer iX = 5,iY=20, iZ = 1;
-        Integer imax = JavaGenerics.maximumInteger(iX,iY,iZ);
-        Assertions.assertEquals(iY,imax);
+        Integer iMax = JavaGenerics.maximumInteger(iX,iY,iZ);
+        Assertions.assertEquals(iY,iMax);
     }
 
     @Test
     public void validZMax() {
         Integer iX = 5,iY=20, iZ = 100;
-        Integer imax = JavaGenerics.maximumInteger(iX,iY,iZ);
-        Assertions.assertEquals(iZ,imax);
+        Integer iMax = JavaGenerics.maximumInteger(iX,iY,iZ);
+        Assertions.assertEquals(iZ,iMax);
     }
+
+    // Test Cased for Float
+    @Test
+    public void validXMaxFloat() {
+        Float xFloat = 11.3f, yFloat = 2.3f, zFloat = 9.5f;
+        Float fMax = JavaGenerics.maximumFloat(xFloat,yFloat,zFloat);
+        Assertions.assertEquals(xFloat,fMax);
+    }
+
+    @Test
+    public void validYMaxFloat() {
+        Float xFloat = 11.3f, yFloat = 21.3f, zFloat = 9.5f;
+        Float fMax = JavaGenerics.maximumFloat(xFloat,yFloat,zFloat);
+        Assertions.assertEquals(yFloat,fMax);
+    }
+
+    @Test
+    public void validZMaxFloat() {
+        Float xFloat = 11.3f, yFloat = 21.3f, zFloat = 91.5f;
+        Float fMax = JavaGenerics.maximumFloat(xFloat,yFloat,zFloat);
+        Assertions.assertEquals(zFloat,fMax);
+    }
+
 }
